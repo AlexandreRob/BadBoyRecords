3dba3b45f648940bb82acdc0ecaa03c27982ddf7
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-08-08 15:16:32 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ from io import BytesIO
 import numpy as np
 import tensorflow as tf
 import keras
+import librosa
 
-# model = keras.models.load_model('./model/')
+model = keras.models.load_model('BadBoyModel.keras')
 
 st.write("""
     # Bad Boy Records
@@ -16,7 +17,9 @@ uploaded_file = st.file_uploader("Upload an song")
 if uploaded_file is not None:
     bytes_data = uploaded_file.read()
 
-    # results = model.predict(bytes_data)
+    music_load = librosa.core.load(bytes_data)
+
+    results = model.predict(music_load)
 
     st.audio(bytes_data, format="audio/wav")
     st.write('Prediction: ')
