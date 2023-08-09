038310870e5554172d6835532862bc86ffae9d6a
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-08-09 16:27:36 +0400

    Changement du model + changement de la transformation

--- app/main.py
+++ app/main.py
@@ -9,20 +9,10 @@ st.write("""
     # Bad Boy Records
 """)
 
-uploaded_file = st.file_uploader("Upload an song")
+uploaded_file = st.file_uploader("Upload an song", type=["wav"])
 if uploaded_file is not None:
-    bytes_data = uploaded_file.read()
-
-    # Créer un fichier temporaire pour enregistrer les données binaires du fichier audio
-    temp_audio_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_audio_file.write(bytes_data)
-    temp_audio_file.close()
+    st.audio(uploaded_file, format="audio/wav")
     
-    predicted_class_name = model_pred(temp_audio_file.name)
+    predict = model_pred(uploaded_file)
 
-    # Supprimer le fichier temporaire
-    os.remove(temp_audio_file.name)
-    
-
-    st.audio(bytes_data, format="audio/wav")
-    st.write(predicted_class_name)
+    st.write(predict)
