40a3039045e67e0de28c013f046363791260ec29
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-08-09 09:22:14 +0400

    Chargement du modèle et import d'un fichier audio depuis streamlit
    Pour le moment le format de fichiers audio n'est pas reconnu après avoir uploadé un échantillon..

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
-import streamlit as st
+import keras
 import numpy as np
 from PIL import Image
+import librosa # Pour l'extraction des features et la lecture des fichiers wav
+import librosa.display # Pour récupérer les spectrogrammes des audio
+import librosa.feature
+import streamlit as st
 from io import BytesIO
-import numpy as np
 import tensorflow as tf
-import keras
+from skimage.transform import resize
 
-# model = keras.models.load_model('./model/')
+model = keras.models.load_model('BadBoyModel.keras')
+
+genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 
+        'jazz', 'metal', 'pop', 'reggae', 'rock']
 
 st.write("""
     # Bad Boy Records
@@ -16,7 +22,27 @@ uploaded_file = st.file_uploader("Upload an song")
 if uploaded_file is not None:
     bytes_data = uploaded_file.read()
 
-    # results = model.predict(bytes_data)
+    y, sr = librosa.load(uploaded_file, mono=True)
+    # Compute the spectrogram of the audio file
+    spectrogram = np.abs(librosa.stft(y))
+    # Resize the spectrogram to match the input shape of the model
+    input_shape = (128, 660)
+    spectrogram = resize(spectrogram, input_shape)
+    # Reshape the spectrogram to match the expected input shape of the model
+    spectrogram = np.expand_dims(spectrogram, axis=-1)
+    spectrogram = np.expand_dims(spectrogram, axis=0)
+    #Voici la liste des genres musicaux représentés dans la notre base de données, on en compte 10.
+    # Make a prediction with the preprocessed audio
+    predictions = model.predict(spectrogram)
+    # Get the predicted class index
+    predicted_class_index = np.argmax(predictions[0])
 
+    # Get the name of the predicted class
+    predicted_class_name = genres[predicted_class_index]
+
+    # Get the predicted class
+    predicted_class = np.argmax(predictions[0])
+    # Display the name of the predicted class
+    print(f'The predicted class is: {predicted_class_name}')
     st.audio(bytes_data, format="audio/wav")
     st.write('Prediction: ')
