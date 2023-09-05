dd7cdce718af1209b10cd83cca92836c112de182
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-09-05 13:37:07 +0400

    Enregistre l'image a partir de la musique de l'utilisateur

--- app/main.py
+++ app/main.py
@@ -14,5 +14,19 @@ if uploaded_file is not None:
     st.audio(uploaded_file, format="audio/wav")
     
     predict = model_pred(uploaded_file)
+        
+    st.write(predict[0])
 
-    st.write(predict)
+    agree = st.checkbox("Si la prédiction n'est pas bonne clique")
+
+    if agree:
+        genre = st.radio("Choisir à quel genre appartient la musique", genres)
+        
+        if genres :
+            
+            st.write(f'Le genre selectionner est: {genre}.')
+            if st.button('envoyer le resultat'):
+                create_img(predict[1],uploaded_file.name)
+                # create_song(uploaded_file)
+        else:
+            st.write("Selectionner un genre.")
